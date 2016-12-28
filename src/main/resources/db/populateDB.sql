ae6a2cc47a5603c0e70bb5c099d443f3828a953f
sergiovanovi <inbox@sergiovanovi.com>
2016-12-28 09:07:41 +0300

    addMealsDBScript

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -11,3 +12,11 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id) VALUES
+  ('2016-12-27 09:00', 'Завтрак', 500, 100000),
+  ('2016-12-27 12:00', 'Обед', 1000, 100000),
+  ('2016-12-27 18:00', 'Ужин', 500, 100000),
+  ('2016-12-28 10:00', 'Завтрак', 500, 100001),
+  ('2016-12-28 13:00', 'Обед', 1000, 100001),
+  ('2016-12-28 19:00', 'Ужин', 500, 100001);
